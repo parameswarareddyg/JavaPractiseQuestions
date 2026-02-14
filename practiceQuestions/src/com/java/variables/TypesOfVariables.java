dd2aa511612d3da4de97896ed5624d89a8d0c42d
parameswarareddyg <gunnampalliparameswarareddy@gmail.com>
2026-02-14 18:30:01 +0530

    Updated Final Variable Lines

--- practiceQuestions/src/com/java/variables/TypesOfVariables.java
+++ practiceQuestions/src/com/java/variables/TypesOfVariables.java
@@ -10,7 +10,7 @@ public class TypesOfVariables {
 	protected short number1 = 300;
 	public short number2 = 400;
 	short number3 = 500;//default
-	
+	private final int value = 2000;
 	
 	//accessmodifier returntype functionname() {} --> object.functionname create chesi access function
 	public void substraction(int i,int j)//non static function or instance function function definition
@@ -32,6 +32,25 @@ public class TypesOfVariables {
 		byte i=10,j=20;
 		System.out.println("Local Variable :  "+name);
 		//output : Local Variable : lokesh
+		
+		
+		//Final Variable
+		final int number;
+		number = 1000;
+		//number = 2000; ---> Unable to edit becuase only once need to initiallize hence it is final variable.
+		int sum=0;
+		System.out.println("Final Variable : "+number);
+	}
+	
+	public void sum(int i,int j)
+	{
+		int sum;
+		sum=0;
+		sum =1;
+		System.out.println("Sum Value before sum() : "+sum);
+		
+		sum=i+j;
+		System.out.println("Sum Value After sum() : "+sum);
 	}
 	
 	public static void main(String[] args) {
@@ -42,6 +61,8 @@ public class TypesOfVariables {
 		System.out.println("Instance Variable : "+obj.number3);
 		obj.substraction(10, 10);//-->Function call
 		display();//-->function Call
+		//		obj.value = 3000;--> Final variable cannot be modified once it is initiallized it can be 
+		//an instance final variable or local final variable or static final variable
 	}
 	
 }
